b30a7ed012ca389f66551156674afd5423504bce
simonlindgren <simon.lindgren@umu.se>
2020-12-03 16:43:23 +0100

    edit imports

--- e2j.py
+++ e2j.py
@@ -2,7 +2,8 @@
 
 import networkx as nx
 from networkx.readwrite import json_graph
-import json, community
+import json
+import community
 from networkx.algorithms import centrality as cn
 from networkx.algorithms import bipartite
 import pandas as pd
@@ -34,8 +35,8 @@ targets = [node_dict.get(s) for s in edgelist.target]
 weights = [w for w in edgelist.weight]
 
 edge_tuples = []
-for s,t,w in zip(sources,targets,weights):
-    edge_tuples.append((s,t,w))
+for s, t, w in zip(sources, targets, weights):
+    edge_tuples.append((s, t, w))
 
 # Make a node names dictionary
 name_dict = {k: v for k, v in zip(nodelist.id, nodelist.name)}
@@ -50,15 +51,15 @@ betweenness = cn.betweenness_centrality(G, weight='weight')
 eigenvector = cn.eigenvector_centrality(G, weight='weight')
 
 # Add node attributes for name, modularity, and three types of centrality.
-nx.set_node_attributes(G, name = 'name', values = name_dict)
-nx.set_node_attributes(G, name = 'group', values = groups)
-nx.set_node_attributes(G, name = 'degree', values = degree)
-nx.set_node_attributes(G, name = 'betweenness', values = betweenness)
-nx.set_node_attributes(G, name = 'eigenvector', values = eigenvector)
+nx.set_node_attributes(G, name='name', values=name_dict)
+nx.set_node_attributes(G, name='group', values=groups)
+nx.set_node_attributes(G, name='degree', values=degree)
+nx.set_node_attributes(G, name='betweenness', values=betweenness)
+nx.set_node_attributes(G, name='eigenvector', values=eigenvector)
 
 # Create json representation of the graph (for d3).
 data = json_graph.node_link_data(G)
 
 # Output json of the graph.
 with open('graph.json', 'w') as output:
-    json.dump(data, output, sort_keys=True, indent=4, separators=(',',':'))
+    json.dump(data, output, sort_keys=True, indent=4, separators=(',', ':'))
